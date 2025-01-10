589bb3069f7f267af00ee08ab9abc8c91dc78716
common khadka <commonkhadka@gmail.com>
2025-01-11 00:09:26 +0545

    Add view and edit mode for personal details

--- CommonVueAuth/src/views/PersonalDetailsView.vue
+++ CommonVueAuth/src/views/PersonalDetailsView.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="rounded overflow-x-hidden shadow-lg p-3" ref="PersonalDetailsForm">
+    <div class="rounded overflow-x-hidden shadow-lg p-3">
+        <div class="border-b-2 border-indigo-500 py-3 flex justify-between items-center">
+            <h4 class="text-xl font-medium text-gray-700 capitalize mb-0">
+                <fa icon="fa fa-user" /> Personal Details
+            </h4>
+            <button @click="ToogleView" :class="`text-sm text-white ${toogleBtnColor} px-3 py-1 rounded-md`">
+                <fa :icon="toogleIcon" /> {{ EditView }}
+            </button>
 
-        <form @submit.prevent="submit" >
+        </div>
+        <form @submit.prevent="submit" ref="PersonalDetailsForm" class="mt-4">
 
             <div class="grid grid-cols-1 md:grid-cols-2  gap-4">
                 <div class="position">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Position</label>
-                        <input v-model="data.position" type="text" required
+                        <input v-model="data.position" type="text" required :disabled="!isEditView" 
                             class="block w-full mt-1 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                             placeholder="Position">
                     </div>
@@ -15,7 +23,7 @@
                 <div class="subName">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Second Name</label>
-                        <input v-model="data.subName" type="text"
+                        <input v-model="data.subName" type="text" :disabled="!isEditView" 
                             class="block w-full mt-1 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                             placeholder="Nickname">
                     </div>
@@ -23,7 +31,7 @@
                 <div class="shortDescription">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Short Description</label>
-                        <textarea v-model="data.shortDescription" type="text"
+                        <textarea v-model="data.shortDescription" type="text" :disabled="!isEditView" 
                             class="block w-full mt-1 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                             placeholder="Short and sweet description"> </textarea>
                     </div>
@@ -31,7 +39,7 @@
                 <div class="longDescription">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Detailed Description</label>
-                        <textarea v-model="data.detailedDescription" type="text"
+                        <textarea v-model="data.detailedDescription" type="text" :disabled="!isEditView" 
                             class="block w-full mt-1 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                             placeholder="Short and sweet description"> </textarea>
                     </div>
@@ -39,7 +47,7 @@
                 <div class="profile">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Profile Picture</label>
-                        <input type="file" @change="handleProfileFileUpload"
+                        <input type="file" @change="handleProfileFileUpload" :disabled="!isEditView" 
                             class="w-full text-gray-400 font-semibold text-sm bg-white border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100 file:hover:bg-gray-200 file:text-gray-500 rounded" />
                         <p class="text-xs text-gray-400 mt-2">PNG and JPG are Allowed.</p>
                     </div>
@@ -50,7 +58,7 @@
                 <div class="banner">
                     <div class="w-full">
                         <label class="text-base text-gray-500 font-semibold mb-2 block">Banner Image</label>
-                        <input type="file" @change="handleBannerFileUpload"
+                        <input type="file" @change="handleBannerFileUpload" :disabled="!isEditView" 
                             class="w-full text-gray-400 font-semibold text-sm bg-white border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100 file:hover:bg-gray-200 file:text-gray-500 rounded" />
                         <p class="text-xs text-gray-400 mt-2">PNG and JPG are Allowed.</p>
                     </div>
@@ -58,16 +66,18 @@
                         <img :src="imagePre.bannerImage" alt="Banner Image" class="h-32 rounded-lg mt-4" />
                     </div>
                 </div>
+
+
+
+            </div>
+            <div v-if="isEditView" class='actions mt-6 flex justify-end'>
+                <button type="submit"
+                    class="px-4 py-2 text-sm text-center text-white bg-indigo-600 rounded-md focus:outline-none hover:bg-indigo-500">
+                    Save
+                </button>
                 
-
-
             </div>
-            <div class='actions mt-6 flex justify-end'>
-                    <button type="submit"  
-                        class="w-full px-4 py-2 text-sm text-center text-white bg-indigo-600 rounded-md focus:outline-none hover:bg-indigo-500 w-1/5">
-                        Save
-                    </button>
-            </div>
+           
         </form>
 
     </div>
@@ -83,6 +93,24 @@ import { afterSlot } from '../composables/afterSlot';
 const $toast = useToast();
 const PersonalDetailsForm = ref(null);
 
+const EditView = ref('');
+const isEditView = ref(false);
+const toogleIcon = ref('fa-edit');
+const toogleBtnColor = ref(null);
+
+
+const ToogleView = () => {
+    isEditView.value = !isEditView.value;
+    EditView.value = isEditView.value ? 'Cancel' : 'Edit';
+    toogleIcon.value = isEditView.value ? "circle-xmark" : "fa-edit";
+    toogleBtnColor.value = isEditView.value ? 'bg-red-500' : 'bg-green-500';
+    if(isEditView.value){
+        toogleIcon.value = "circle-xmark";
+    }else{
+        fetchData();
+    }
+}
+
 const imagePre = reactive({
     bannerImage: null,
     profilePicture: null,
@@ -94,11 +122,17 @@ const data = reactive({
     position: null,
     shortDescription: null,
     detailedDescription: null
-
-
 });
 
 onMounted(async () => {
+    EditView.value = "Edit";
+    toogleIcon.value = "fa-edit";
+    toogleBtnColor.value = "bg-green-500";
+    fetchData();
+    
+
+});
+async function fetchData() {
     loader.BlockWindow(PersonalDetailsForm.value, afterSlot());
 
     var response = await axios.get("/api/account-details");
@@ -111,7 +145,7 @@ onMounted(async () => {
         data.subName = responseData.subName;
         data.shortDescription = responseData.shortDescription;
         data.detailedDescription = responseData.detailedDescription;
-        
+
         imagePre.bannerImage = responseData.bannerPictureLink;
         imagePre.profilePicture = responseData.profilePictureLink;
         loader.UnBlockWindow();
@@ -119,8 +153,7 @@ onMounted(async () => {
         loader.UnBlockWindow();
         $toast.error(response.data.reason, 'Error');
     }
-
-});
+}
 
 const submit = async () => {
 
