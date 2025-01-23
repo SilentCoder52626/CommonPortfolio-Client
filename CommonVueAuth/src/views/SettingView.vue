761a846361a09f3b4b114970c601fc5107244ae4
common khadka <commonkhadka@gmail.com>
2025-01-24 01:38:49 +0545

    Add setting view

--- CommonVueAuth/src/views/SettingView.vue
+++ CommonVueAuth/src/views/SettingView.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ChangePassword />
+        <Setting />
 
     </div>
 </template>
 
 <script setup>
 import ChangePassword from '../components/users/ChangePassword.vue';
-
+import Setting from '../components/users/Setting.vue';
 
 
 </script>
