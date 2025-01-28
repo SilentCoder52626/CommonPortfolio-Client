82989911a264cc7facaa8b61a02f6a855008d357
Kaman Khadka <commonkhadka@gmail.com>
2025-01-28 13:03:52 +0545

    Updated toolbar actions

--- CommonVueAuth/src/components/MdEditorWrapper.vue
+++ CommonVueAuth/src/components/MdEditorWrapper.vue
@@ -4,11 +4,37 @@
   
   <script setup>
   import { MdEditor } from 'md-editor-v3';
-  const toolbars = ['bold', 'underline', 'italic','title', 'quote','unordered list','ordered list'];
-
+  const toolbars = [
+  'bold',
+  'underline',
+  'italic',
+  '-',
+  'strikeThrough',
+  'title',
+  'sub',
+  'sup',
+  'quote',
+  'unorderedList',
+  'orderedList',
+  'task',
+  '-',
+  'codeRow',
+  'code',
+  'link',
+  'table',
+  'mermaid',
+  'katex',
+  '=',
+  'pageFullscreen',
+  'fullscreen',
+  'preview',
+  'previewOnly',
+  'catalog'
+];
   const editorProps = {
     language: 'en-US',
     showToolbarName : false,
+    toolbars : toolbars,
   };
   </script>
   
